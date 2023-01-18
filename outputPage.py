a934fb254588fc8acb1d9f72d2b34555c32ed93e
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 12:40:25 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -25,11 +25,11 @@ returnMood()
 
            
 res2 = values.split("fav=", 1)
-#res3 = values.split("fav=", 1)
+res3 = values.split("time=", 1)
 #res4 = values.split("time=", 1)
 #for_child= be.decode_string(value)
 #mood = be.decode_string(mood)
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write( res2[1], res2[5] )
+st.write( res2[1] + res3[1] )
