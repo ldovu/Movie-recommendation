a9ee8e843c98c76a3d0aa1c78a66256fde9353a3
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 11:32:18 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -3,10 +3,9 @@ import backend as be
 #get query parameters
 values = st.experimental_get_query_params()["value"][0]
 
-
 #for_child= be.decode_string(value)
 #mood = be.decode_string(mood)
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write(values.partition('mood=')[1])
+st.write(values.partition())
