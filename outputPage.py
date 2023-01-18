b7e4c9b31a08f1ac3eb5f3d0d29f554c943931af
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 10:24:37 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -1,12 +1,18 @@
 import streamlit as st
-#import backend as be
-from streamlit_javascript import st_javascript
+import backend as be
 
 #get query parameters
+if st.experimental_get_query_params():
+    value = st.experimental_get_query_params()["value"][0]
+    
+    
+   # mood =  st.experimental_get_query_params()["mood"][0]
+   # fav =  st.experimental_get_query_params()["fav"][0]
+    #time =   st.experimental_get_query_params()["time"][0]
 
+for_child= be.decode_string(value)
+#mood = be.decode_string(mood)
+#film_target =be.decode_string(fav)
+#Max = be.decode_string(time)
 
-url = st_javascript("await fetch('').then(r => window.parent.location.href)")
-
-st.write(url)
-
-
+st.write( for_child )
