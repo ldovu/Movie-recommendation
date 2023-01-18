4be99d527543317fb75a078175caa09626a1315e
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 19:26:07 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -8,10 +8,14 @@ values = st.experimental_get_query_params()["qwerty"][0]
 
 list1 = values.split('/?')
 
-for_kids = list1[0].replace("qwerty=", "")
-mood= list1[1].replace("asdfgh=", "")
+child = list1[0].replace("qwerty=", "")
+mood = list1[1].replace("asdfgh=", "")
 film_target = list1[2].replace("zxcvbn=", "").replace("%20", " ")
-Tmax=list1[3].replace("time=", "")
+time =list1[3].replace("time=", "")
+
+for_kids = True
+Tmax= int(time)
+
 
 m = mrs.MovieRecommendationSystem()
 recommended = m.recommend(for_kids, mood, film_target, Tmax)
