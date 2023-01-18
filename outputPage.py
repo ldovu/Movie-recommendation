4c2efc13461e50525e5242c861a03a002a1fb725
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 09:43:28 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import backend as be
-import inputPage 
+import inputPage
+import MovieRecommendationSystem as mrs 
 
 
 #get query parameters
-#if st.experimental_get_query_params():
-  #  value = st.experimental_get_query_params()["value"][0]
-#st.session_state.child = be.decode_string(value)
+if st.experimental_get_query_params():
+    value = st.experimental_get_query_params()["value"][0]
+    mood =  st.experimental_get_query_params()["mood"][0]
+    fav =  st.experimental_get_query_params()["fav"][0]
+    time =   st.experimental_get_query_params()["time"][0]
 
-st.title("Here we go")
-st.write("You are a" )
-         #st.sessione_state.child
+for_child= be.decode_string(value)
+mood = be.decode_string(mood)
+film_target =be.decode_string(fav)
+Max = be.decode_string(time)
+
+st.write("You are a" + for_child + " mood of " +mood+ 
+         " closest film " + film_target+ " max time" + Max )
