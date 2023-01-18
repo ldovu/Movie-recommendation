5c7cc8f227b612cc6a53e2815ba65e3bf8fae4f2
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 17:17:19 +0100

    Update chooseChildAdult.py

--- chooseChildAdult.py
+++ chooseChildAdult.py
@@ -7,16 +7,26 @@ def nav_to(url):
     st.write(nav_script, unsafe_allow_html=True)
 
 
-
-col1, col2, col3 = st.columns([1, 6,1])
+col1, col2, col3 = st.columns([1, 6 ,1])
 with col2:
-    with st.form(""):
-        st.header("Let's start with your age")
-        st.radio("Are you an adult o a child?", ["adult", "child"], key="qwerty")
-        first = st.form_submit_button("Start")
-        if first:
+    st.header("Let's start with your age")
+    st.radio("Are you an adult o a child?", ["adult", "child"], key="qwerty")
+    
+    chat_botton = st.write(f'''
+                                 <div class="div">
+                                     <center>
+                                         <a href="/?qwerty=%s">
+                                             <button> Move to the next question </button>
+                                         </a>
+                                     </center>
+                                 <div class="btn">
+                                      
+                                ''' % (st.session_state.qwerty), unsafe_allow_html=True)
+
+    if chat_botton() :
             if st.session_state.qwerty =="adult":
                 nav_to("link to the page of the adult with mood")
             elif st.session_state.qwerty =="child":
                 nav_to("link of the page of the child without mood")
+
 
