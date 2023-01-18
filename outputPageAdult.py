55a1269b6ff398404f66e395711e9f8839e024e3
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 19:06:01 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -1,20 +1,17 @@
 import streamlit as st
-import backend as be
 import pandas as pd
+import MovieRecommendationSystem as mrs
+
 
 #get query parameters
 values = st.experimental_get_query_params()["qwerty"][0]
 
 list1 = values.split('/?')
 
-for_child = list1[0].replace("qwerty=", "")
+for_kids = list1[0].replace("qwerty=", "")
 mood= list1[1].replace("asdfgh=", "")
-fav = list1[2].replace("zxcvbn=", "").replace("%20", " ")
-time=list1[3].replace("time=", "")
+film_target = list1[2].replace("zxcvbn=", "").replace("%20", " ")
+Tmax=list1[3].replace("time=", "")
 
-
-
-st.write(for_child)
-st.write(mood)
-st.write(fav)
-st.write(time)
+m = mrs.MovieRecommendationSystem()
+recommended = m.recommend(for_kids, mood, film_target, Tmax)
