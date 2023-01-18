397fa6a8f864b05cfd3d6a9d542ea7553ae43975
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 18:25:38 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -3,14 +3,15 @@ import backend as be
 import pandas as pd
 
 #get query parameters
-values = st.experimental_get_query_params()["asdfgh"][0]
+values = st.experimental_get_query_params()["qwerty"][0]
 
 list1 = values.split('/?')
 
-for_child = "adult"
-mood= list1[0].replace("asdfgh=", "")
-fav = list1[1].replace("zxcvbn=", "").replace("%20", " ")
-time=list1[2].replace("time=", "")
+for_child = list1[0].replace("qwerty=", "")
+mood= list1[1].replace("asdfgh=", "")
+fav = list1[2].replace("zxcvbn=", "").replace("%20", " ")
+time=list1[3].replace("time=", "")
+
 
 
 st.write(for_child)
