415de2b16cc9afe85c89f0af59ea21a1d706d949
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 11:34:14 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -2,10 +2,10 @@ import streamlit as st
 import backend as be
 #get query parameters
 values = st.experimental_get_query_params()["value"][0]
-
+a = str(values)
 #for_child= be.decode_string(value)
 #mood = be.decode_string(mood)
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write(values.partition())
+st.write(a.partition())
