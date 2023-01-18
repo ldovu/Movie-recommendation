b068a44220ea19778f7c105df5ff7d3979bdb02e
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 13:34:42 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -5,4 +5,17 @@ import pandas as pd
 #get query parameters
 values = st.experimental_get_query_params()["value"][0]
 
-st.write(values.split('/?'))
+
+
+list1 = values.split('/?')
+
+for_child = list1[0].replace("value=", "")
+mood= list1[1].replace("mood=", "")
+fav = list1[2].replace("fav=", "").replace("%20", " ")
+time=list1[3].replace("time=", "")
+
+
+st.write(for_child)
+st.write(mood)
+st.write(fav)
+st.write(time)
