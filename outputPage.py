fb62f9f3e8f9c3cb790d9765d0cd7ecad1d3cd3c
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 09:58:43 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import backend as be
+#import backend as be
 
 
 #get query parameters
-st.experimental_get_query_params():
+st.experimental_get_query_params()
 
 '''
     value = st.experimental_get_query_params()["value"][0]
