ff4f4a3a9c4769adb803e107fa92d93720e0fea6
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 10:38:36 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -2,9 +2,8 @@ import streamlit as st
 import backend as be
 
 #get query parameters
-if st.experimental_get_query_params():
-    for_child = st.experimental_get_query_params()["value"][0]
-    mood =  st.experimental_get_query_params()["mood"][0]
+for_child = st.experimental_get_query_params()["value"][0]
+    #mood =  st.experimental_get_query_params()["mood"][0]
    # fav =  st.experimental_get_query_params()["fav"][0]
     #time =   st.experimental_get_query_params()["time"][0]
 
@@ -13,4 +12,4 @@ if st.experimental_get_query_params():
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write( for_child , mood)
+st.write( for_child )
