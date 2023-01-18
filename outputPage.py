eacc75df13e86f68e4f260d8cd60ed0ac7fa253a
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 13:08:29 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -5,31 +5,4 @@ import pandas as pd
 #get query parameters
 values = st.experimental_get_query_params()["value"][0]
 
-def returnAge():
-    x =["child","adult"]
-    for i in x:
-        if i in values:
-            for_child=i
-            return st.write(for_child)
-returnAge()
-    
-def returnMood():
-    y= ["RIDERE","PIANGERE", "ADRENALINA", "AVVENTURA","AMORE", "PAURA", "FANTASTICARE", "SCIENCE FICTION", "CASUAL"]
-    for i in y:
-        if i in values:
-            mood = i
-            return st.write(mood)
-returnMood()
-
-        
-
-           
-res2 = values.split("fav=", 1)
-res3 = values.split("time=", 1)
-#res4 = values.split("time=", 1)
-#for_child= be.decode_string(value)
-#mood = be.decode_string(mood)
-#film_target =be.decode_string(fav)
-#Max = be.decode_string(time)
-
-st.write( res2[1].replace("/?time=", "") + res3[1]  )
+st.write(values.split('/?'))
