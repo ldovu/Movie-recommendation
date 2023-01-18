fdd3e48516d05a9b1adc2bf242b4798fe7ca6dbf
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 10:00:07 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -5,17 +5,3 @@ import streamlit as st
 #get query parameters
 st.experimental_get_query_params()
 
-'''
-    value = st.experimental_get_query_params()["value"][0]
-    mood =  st.experimental_get_query_params()["mood"][0]
-    fav =  st.experimental_get_query_params()["fav"][0]
-    time =   st.experimental_get_query_params()["time"][0]
-
-for_child= be.decode_string(value)
-mood = be.decode_string(mood)
-film_target =be.decode_string(fav)
-Max = be.decode_string(time)
-
-st.write("You are a" + for_child + " mood of " +mood+ 
-         " closest film " + film_target+ " max time" + Max )
-'''
