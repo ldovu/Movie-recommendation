563842e659c0852647a6b1b0c0e35e5fac1f412a
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 11:54:38 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -4,16 +4,19 @@ import backend as be
 #get query parameters
 values = st.experimental_get_query_params()["value"][0]
 
-
-res1 = values.split("value=", 1)
+x ="child"
+y = "adult"
+if x in values:
+    st.write("user is a" +x)
+if y in values:
+    st.write("user is a" +y)
+            
 res2 = values.split("mood=", 1)
-res3 = values.split("fav=", 1)
-res4 = values.split("time=", 1)
+#res3 = values.split("fav=", 1)
+#res4 = values.split("time=", 1)
 #for_child= be.decode_string(value)
 #mood = be.decode_string(mood)
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write("for_child" + res1[1] )
-         
-         #+ " \n mood" + res2[1] + "\n sim to " + res3[1]+  "\n time"+res4[1] 
+st.write("for_child" + res2[1] )
