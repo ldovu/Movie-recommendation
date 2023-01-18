dbd2d2392d15bc3643cb04896390a3c17eed936b
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-18 10:36:15 +0100

    Update outputPage.py

--- outputPage.py
+++ outputPage.py
@@ -4,9 +4,7 @@ import backend as be
 #get query parameters
 if st.experimental_get_query_params():
     for_child = st.experimental_get_query_params()["value"][0]
-    
-    
-   # mood =  st.experimental_get_query_params()["mood"][0]
+    mood =  st.experimental_get_query_params()["mood"][0]
    # fav =  st.experimental_get_query_params()["fav"][0]
     #time =   st.experimental_get_query_params()["time"][0]
 
@@ -15,4 +13,4 @@ if st.experimental_get_query_params():
 #film_target =be.decode_string(fav)
 #Max = be.decode_string(time)
 
-st.write( for_child )
+st.write( for_child , mood)
