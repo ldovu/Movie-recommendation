d85a6fed274c42f315233f4f659fed1622d9b6ba
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-19 23:57:37 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -22,12 +22,6 @@ m = mrs.MovieRecommendationSystem()
 recommended = m.recommend(for_kids, mood, film_target , Tmax )
 df = recommended.reset_index(drop=True)
 
-    
- #if di.checkTitolo(film_target) or film_target=="":
-  #  else:
-   # st.error("You insert a wrong movie title, please go back and provide a new one", icon="🚨")
-        
-
 
 #prova link per i link delle trame dei film 
 #url = "https://ldovu-movie-recommendation-page0-awd4vf.streamlit.app/"
