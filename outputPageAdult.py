3b9ef9dda31c9016d87717140475cef5b7718660
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-19 17:46:16 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -17,15 +17,17 @@ time =list1[3].replace("time=", "")
 for_kids = False
 Tmax = int(time)
 
+if di.checkTitolo(film_target) or film_target=="":
+    for_kids = False
+    Tmax = int(time)
 
-if di.checkTitolo(film_target)== False:
-    st.error("You insert a wrong movie title, please go back and provide a new correct one", icon="🚨")
-    
-else:
     m = mrs.MovieRecommendationSystem()
     recommended = m.recommend(for_kids, mood, film_target, Tmax)
     df = recommended.reset_index(drop=True)
         
+else:
+    st.error("You've inserted a wrong movie title, please go back and provide a new one", icon="🚨")
+
 
 
 #prova link per i link delle trame dei film 
