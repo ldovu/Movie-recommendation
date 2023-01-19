7c6f7cd9200dc44ed6c5617a2178e2045c37fb35
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-19 17:38:25 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import MovieRecommendationSystem as mrs
+import DatasetInspection as di
 
 
 #get query parameters
@@ -13,22 +14,22 @@ mood = list1[1].replace("asdfgh=", "")
 film_target = list1[2].replace("zxcvbn=", "").replace("%20", " ")
 time =list1[3].replace("time=", "")
 
-for_kids = False
-Tmax = int(time)
+if di.checkTitolo(film_target) or film_target=="":
+    for_kids = False
+    Tmax = int(time)
 
-m = mrs.MovieRecommendationSystem()
-recommended = m.recommend(for_kids, mood, film_target, Tmax)
-df = recommended.reset_index(drop=True)
+    m = mrs.MovieRecommendationSystem()
+    recommended = m.recommend(for_kids, mood, film_target, Tmax)
+    df = recommended.reset_index(drop=True)
+        
+else:
+    st.warning("You insert a wrong movie title, please go back and provide a new correct one", icon="⚠️")
 
-st.write(df )
-
-st.write(for_kids)
-st.write(mood)
-st.write(film_target)
-st.write(time)
 
 #prova link per i link delle trame dei film 
 #url = "https://ldovu-movie-recommendation-page0-awd4vf.streamlit.app/"
 #st.write("check out this [link](%s)" % url)
+
+
 
 
