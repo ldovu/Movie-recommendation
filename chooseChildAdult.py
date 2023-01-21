cbf91139147bf5c50f05c0441d31cf7cae6c33d1
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-21 10:34:09 +0100

    Update chooseChildAdult.py

--- chooseChildAdult.py
+++ chooseChildAdult.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+
+class Person:
+    def __init__(self, age):
+        self.a = age
+
+user1= Person("child")
+user2= Person("adult")
+
 def nav_to(url):
     nav_script = """
                     <meta http-equiv="refresh" content="0; url='%s'">
@@ -12,11 +20,11 @@ with col2:
     st.header("Let's start with your age")
     st.radio("Are you an adult o a child?", ["adult", "child"], key="qwerty")
     
-    if st.session_state.qwerty =="adult":
+    if st.session_state.qwerty == user2.a:
         chat_botton1 = st.write(f'''
                                      <div class="div">
                                          <center>
-                                             <a href="https://ldovu-movie-recommendation-inputpageadult-ksx813.streamlit.app/?qwerty=%s">
+                                             <a href="https://ldovu-movie-recommendation-inputpageadult-45z3ld.streamlit.app/?qwerty=%s">
                                                  <button> Move to the next question </button>
                                              </a>
                                          </center>
@@ -25,7 +33,7 @@ with col2:
                                     ''' % (st.session_state.qwerty), unsafe_allow_html=True)
         if chat_botton1 :
                 nav_to("https://ldovu-movie-recommendation-inputpageadult-45z3ld.streamlit.app/")
-    elif st.session_state.qwerty =="child":
+    elif st.session_state.qwerty == user1.a :
         chat_botton2 = st.write(f'''
                                      <div class="div">
                                          <center>
@@ -40,5 +48,6 @@ with col2:
                 nav_to("https://ldovu-movie-recommendation-inputpagechild-2jx062.streamlit.app/")
                 
                
+  
 
 
