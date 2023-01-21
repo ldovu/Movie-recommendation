beee7fb19d1f521a21c88095c481b4647e2e3b4f
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-21 17:17:45 +0100

    Update outputPageAdult.py

--- outputPageAdult.py
+++ outputPageAdult.py
@@ -21,7 +21,17 @@ m = mrs.MovieRecommendationSystem()
 recommended = m.recommend(for_kids, mood, film_target , Tmax )
 
 
+
+df = recommended.reset_index(drop=True)
+st.write(df)
+
+
+
 '''
+#prova link per i link delle trame dei film 
+#url = "https://ldovu-movie-recommendation-page0-awd4vf.streamlit.app/"
+#st.write("check out this [link](%s)" % url)
+
 for row in recommended.iterrows():
     for y in di.trovaOverview(row):
         overviewColumn={row:y}
@@ -29,14 +39,6 @@ for row in recommended.iterrows():
 # Using 'Address' as the column name and equating it to the list
 df = recommended.assign(overview =overviewColumn)
 '''
-
-
-df = recommended.reset_index(drop=True)
-st.write(df)
-
-#prova link per i link delle trame dei film 
-#url = "https://ldovu-movie-recommendation-page0-awd4vf.streamlit.app/"
-#st.write("check out this [link](%s)" % url)
 
 
 
