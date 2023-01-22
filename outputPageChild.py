e820f82d28a4d770acb7857f7c80a4596f7aa477
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-22 10:48:17 +0100

    Update outputPageChild.py

--- outputPageChild.py
+++ outputPageChild.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import MovieRecommendationSystem as mrs
+import numpy as np
+import DatasetInspection as di
 
 #get query parameters
 values = st.experimental_get_query_params()["qwerty"][0]
@@ -8,9 +10,10 @@ values = st.experimental_get_query_params()["qwerty"][0]
 list1 = values.split('/?')
 
 child = list1[0].replace("qwerty=", "")
+mood = ""
 film_target = list1[1].replace("zxcvbn=", "").replace("%20", " ")
 time =list1[2].replace("time=", "")
-mood = ""
+
 
 for_kids = True
 Tmax = int(time)
@@ -18,4 +21,16 @@ Tmax = int(time)
 
 m = mrs.MovieRecommendationSystem()
 recommended = m.recommend(for_kids, mood,film_target, Tmax)
-st.write(recommended )
+
+listRecom = recommended['title'].values
+
+listOverview = []
+
+for i in range(len(listRecom)): #iterate over rows 
+    value = di.trovaOverview(listRecom[i])
+    listOverview.append(value)
+    st.write(listRecom[i])
+    
+    expander = st.expander("See the overview")
+    expander.write(listOverview[i] )
+    
