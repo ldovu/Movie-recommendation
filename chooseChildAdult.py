88c1a02bb63de5232dd49a729c0f1288e3480d04
ldovu <115782431+ldovu@users.noreply.github.com>
2023-01-26 16:39:12 +0100

    Update chooseChildAdult.py

--- chooseChildAdult.py
+++ chooseChildAdult.py
@@ -3,7 +3,7 @@ import streamlit as st
 tabs_font_css = """
                 <style> 
                     .btn{
-                        border-radius: 10px;
+                        border-radius: 5px;
                         box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
                       
                     }
